66c04b50f1ae640dc7bcebf3f844eae0165282d5
Приходько Владислав <3885vlad@gmail.com>
2024-07-23 13:12:07 +0300

    Configured mapping

--- Booking/Booking.WebApi/Program.cs
+++ Booking/Booking.WebApi/Program.cs
@@ -1,11 +1,22 @@
+using AutoMapper;
 using Booking.Application;
+using Booking.Application.Common.Mappings;
+using Booking.Application.Interfaces;
 using Booking.Persistence;
 using Booking.WebApi.Middleware;
 using Notes.Persistence;
+using System.Reflection;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddAutoMapper(
+	configAction => {
+		configAction.AddProfile(new AssemblyMappingProfile(Assembly.GetExecutingAssembly()));
+		configAction.AddProfile(new AssemblyMappingProfile(typeof(IBookingDbContext).Assembly));
+	}
+);
+
 builder.Services.AddApplication();
 builder.Services.AddPersistence(builder.Configuration);
 

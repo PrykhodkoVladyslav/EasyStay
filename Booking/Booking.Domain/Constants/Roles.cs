9a4323712b092c5e3e2c1ba0b9c422de0e6efbb4
Приходько Владислав <3885vlad@gmail.com>
2024-07-26 18:32:22 +0300

    Added roles

---
+++ Booking/Booking.Domain/Constants/Roles.cs
@@ -0,0 +1,13 @@
+﻿namespace Booking.Domain.Constants;
+
+public static class Roles {
+	public const string Admin = "Admin";
+	public const string Realtor = "Realtor";
+	public const string User = "User";
+
+	public static readonly IReadOnlyList<string> All = [
+		Admin,
+		Realtor,
+		User
+	];
+}

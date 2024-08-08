a58e3d8c61366fb90317cf93af233340d537ddb7
Владислав Приходько <3885vlad@gmail.com>
2024-08-08 13:27:41 +0300

    Improved CustomerItemVm and RealtorItemVm

--- Booking/Booking.Application/MediatR/Accounts/Commands/GetRealtorPage/RealtorItemVm.cs
+++ Booking/Booking.Application/MediatR/Accounts/Commands/GetRealtorPage/RealtorItemVm.cs
@@ -4,6 +4,12 @@ using Booking.Domain.Identity;
 namespace Booking.Application.MediatR.Accounts.Commands.GetRealtorPage;
 
 public class RealtorItemVm : IMapWith<Realtor> {
+	public long Id { get; set; }
+
+	public string Email { get; set; } = null!;
+
+	public string UserName { get; set; } = null!;
+
 	public string FirstName { get; set; } = null!;
 
 	public string LastName { get; set; } = null!;

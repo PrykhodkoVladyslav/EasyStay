ece008cce38a4660aac883b006ba43acf28dd1b6
Владислав Приходько <3885vlad@gmail.com>
2024-08-08 13:15:31 +0300

    Created AccountsController.CreateAdminAsync

--- Booking/Booking.Application/Models/Accounts/UserDto.cs
+++ Booking/Booking.Application/Models/Accounts/UserDto.cs
@@ -1,10 +1,8 @@
-﻿using Booking.Application.Common.Mappings;
-using Booking.Application.MediatR.Accounts.Commands.Registration;
-using Microsoft.AspNetCore.Http;
+﻿using Microsoft.AspNetCore.Http;
 
 namespace Booking.Application.Models.Accounts;
 
-public class UserDto : IMapWith<RegistrationCommand> {
+public class UserDto {
 	public string FirstName { get; set; } = null!;
 	public string LastName { get; set; } = null!;
 	public IFormFile Image { get; set; } = null!;

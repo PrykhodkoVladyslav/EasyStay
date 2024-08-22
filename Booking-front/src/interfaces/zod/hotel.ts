04597fb7a071a81f53dc232b8b10ac54b5777ca0
Oleksandr Putsyk <sashaputsyk100903@gmail.com>
2024-08-22 18:26:16 +0300

    Some updates

--- Booking-front/src/interfaces/zod/hotel.ts
+++ Booking-front/src/interfaces/zod/hotel.ts
@@ -50,27 +50,6 @@ export const HotelCreateSchema = z.object({
 
 export type HotelCreateSchemaType = z.infer<typeof HotelCreateSchema>;
 
-export const CountryCreateSchema = z.object({
-    name: z.string().min(1, "Name is required"),
-    image: z
-        .any()
-        .transform((files) => (files ? Array.from(files) : []))
-        .refine((files: any[]) => files.length > 0, `Image is required.`)
-        .refine((files: any[]) => files.length <= 1, `Max photo count is 1.`)
-        .refine(
-            (files: any[]) => files.length === 0 || files.every((file) => file.size <= MAX_FILE_SIZE),
-            `Max file size is 5MB.`,
-        )
-        .refine(
-            (files: any[]) =>
-                files.length === 0 || files.every((file) => ACCEPTED_IMAGE_MIME_TYPES.includes(file.type)),
-            "Only .jpg, .jpeg, .png and .webp files are accepted.",
-        ),
-});
-
-export type CountryCreateSchemaType = z.infer<typeof CountryCreateSchema>;
-
-
 // export const RoomCreateSchema = z.object({
 //     name: z.string().min(1, "Name is required"),
 //     price: z.string().refine(

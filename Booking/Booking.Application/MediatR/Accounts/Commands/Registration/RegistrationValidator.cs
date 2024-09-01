2ec93c6c6b8eb7175a062167f38c30663da74086
Владислав Приходько <3885vlad@gmail.com>
2024-09-01 12:15:59 +0300

    Refactoring of RegistrationValidator.BuildTypeError method

--- Booking/Booking.Application/MediatR/Accounts/Commands/Registration/RegistrationValidator.cs
+++ Booking/Booking.Application/MediatR/Accounts/Commands/Registration/RegistrationValidator.cs
@@ -1,7 +1,6 @@
 ﻿using Booking.Application.Interfaces;
 using Booking.Domain.Constants;
 using FluentValidation;
-using System.Text;
 
 namespace Booking.Application.MediatR.Accounts.Commands.Registration;
 
@@ -55,22 +54,8 @@ public class RegistrationValidator : AbstractValidator<RegistrationCommand> {
 	}
 
 	private static string BuildTypeError() {
-		var errorBuilder = new StringBuilder();
+		var validTypes = string.Join(", ", Enum.GetNames(typeof(RegistrationUserType)));
 
-		errorBuilder.Append("Type is not valid. Valid types: [ ");
-
-		bool isNotFirst = false;
-		foreach (var typeName in Enum.GetNames(typeof(RegistrationUserType))) {
-			if (isNotFirst)
-				errorBuilder.Append(", ");
-
-			errorBuilder.Append(typeName);
-
-			isNotFirst = true;
-		}
-
-		errorBuilder.Append(" ]");
-
-		return errorBuilder.ToString();
+		return $"Type is not valid. Valid types: [ {validTypes} ]";
 	}
 }

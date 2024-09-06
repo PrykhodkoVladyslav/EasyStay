5caefe37b46c60f556f0d2fbac190a9c8df16eb8
Владислав Приходько <3885vlad@gmail.com>
2024-09-06 16:09:10 +0300

    Added IsLocked field to CustomerItemVm and RealtorItemVm

--- Booking/Booking.Application/MediatR/Accounts/Queries/GetRealtorPage/RealtorItemVm.cs
+++ Booking/Booking.Application/MediatR/Accounts/Queries/GetRealtorPage/RealtorItemVm.cs
@@ -1,4 +1,6 @@
-﻿using Booking.Application.Common.Mappings;
+﻿using AutoMapper;
+using Booking.Application.Common.Mappings;
+using Booking.Application.MediatR.Accounts.Queries.GetCustomerPage;
 using Booking.Domain.Identity;
 
 namespace Booking.Application.MediatR.Accounts.Queries.GetRealtorPage;
@@ -15,4 +17,16 @@ public class RealtorItemVm : IMapWith<Realtor> {
 	public string LastName { get; set; } = null!;
 
 	public string Photo { get; set; } = null!;
+
+	public bool IsLocked { get; set; }
+
+
+
+	public void Mapping(Profile profile) {
+		profile.CreateMap<Realtor, RealtorItemVm>()
+			.ForMember(
+				dest => dest.IsLocked,
+				opt => opt.MapFrom(src => src.LockoutEnd != null && src.LockoutEnd >= DateTimeOffset.UtcNow)
+			);
+	}
 }

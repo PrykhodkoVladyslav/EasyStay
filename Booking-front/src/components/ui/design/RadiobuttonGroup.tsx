5f18e61f5b8255407e7231704d36b4b97363fe41
Владислав Приходько <3885vlad@gmail.com>
2024-09-06 19:21:08 +0300

    Completed RegisterPage page

--- Booking-front/src/components/ui/design/RadiobuttonGroup.tsx
+++ Booking-front/src/components/ui/design/RadiobuttonGroup.tsx
@@ -1,4 +1,6 @@
 import { UseFormRegisterReturn } from "react-hook-form";
+import { ChangeEvent, useState } from "react";
+import { getPublicResourceUrl } from "utils/publicAccessor.ts";
 
 const RadiobuttonGroup = (props: {
     formRegisterReturn?: UseFormRegisterReturn,
@@ -8,19 +10,41 @@ const RadiobuttonGroup = (props: {
         value: string
     }>
 }) => {
+    const [checkedValue, setCheckedValue] = useState("");
+
+    const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
+        console.log(event.target.value);
+        setCheckedValue(event.target.value);
+    };
+
     const className = props.isError
         ? "radiobutton-title-error"
         : "radiobutton-title-default";
 
+    const pathToCheckedImage = getPublicResourceUrl(props.isError
+        ? "radiobutton/circle-filled-red.svg"
+        : "radiobutton/circle-filled-black.svg");
+
+    const pathToUncheckedImage = getPublicResourceUrl(props.isError
+        ? "radiobutton/circle-empty-red.svg"
+        : "radiobutton/circle-empty-black.svg");
+
     return (
         <div className="flex flex-row justify-evenly">
             {props.options.map((option) => (
-                <label className={`radiobutton-title ${className}`}>
+                <label key={option.value} className={`radiobutton-item ${className}`}>
                     <input
                         type="radio"
-                        className="radiobutton-title-margin"
+                        className="hidden"
                         value={option.value}
+                        onChange={handleChange}
+                        onClick={() => setCheckedValue(option.value)}
                         {...props.formRegisterReturn}
+                    />
+                    <img
+                        className="radiobutton-icon"
+                        src={checkedValue === option.value ? pathToCheckedImage : pathToUncheckedImage}
+                        alt="icon"
                     />
                     {option.title}
                 </label>

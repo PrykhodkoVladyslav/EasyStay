66333326679bb472332c4e26ed45bc5ce53ba4e8
Oleksandr Putsyk <sashaputsyk100903@gmail.com>
2024-09-15 20:42:51 +0300

    Deleted some files

--- Booking-front/src/components/guards/ProtectedRoute.tsx
+++ Booking-front/src/components/guards/ProtectedRoute.tsx
@@ -3,11 +3,17 @@ import { useAppSelector } from "store/index.ts";
 import { getToken } from "store/slice/userSlice.ts";
 import { checkToken } from "utils/checkToken.ts";
 
-const ProtectedRoute = () => {
+const AdminProtectedRoute = () => {
     const location = useLocation();
     const token = useAppSelector(getToken);
+    const payload = token ? JSON.parse(atob(token.split('.')[1])) : null;
+    const role = payload ? payload["http://schemas.microsoft.com/ws/2008/06/identity/claims/role"] : null;
 
-    return checkToken(token) ? <Outlet /> : <Navigate to="/auth/login" state={{ from: location }} replace />;
+    if (!checkToken(token) || role !== "Admin") {
+        return <Navigate to="/auth/login" state={{ from: location }} replace />;
+    }
+
+    return <Outlet />;
 };
 
-export default ProtectedRoute;
+export default AdminProtectedRoute;

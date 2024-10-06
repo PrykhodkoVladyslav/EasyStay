cfcd783c888088f839bb00306fc8bba514edb760
Владислав Приходько <3885vlad@gmail.com>
2024-10-06 14:01:52 +0300

    Created queries of RoomType

---
+++ EasyStay/EasyStay.Application/MediatR/RoomTypes/Queries/GetAll/GetAllRoomTypesQueryHandler.cs
@@ -0,0 +1,23 @@
+﻿using AutoMapper;
+using AutoMapper.QueryableExtensions;
+using EasyStay.Application.Interfaces;
+using EasyStay.Application.MediatR.RoomTypes.Queries.Shared;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace EasyStay.Application.MediatR.RoomTypes.Queries.GetAll;
+
+public class GetAllRoomTypesQueryHandler(
+	IEasyStayDbContext context,
+	IMapper mapper
+) : IRequestHandler<GetAllRoomTypesQuery, IEnumerable<RoomTypeVm>> {
+
+	public async Task<IEnumerable<RoomTypeVm>> Handle(GetAllRoomTypesQuery request, CancellationToken cancellationToken) {
+		var items = await context.RoomTypes
+			.AsNoTracking()
+			.ProjectTo<RoomTypeVm>(mapper.ConfigurationProvider)
+			.ToArrayAsync(cancellationToken);
+
+		return items;
+	}
+}
---
+++ EasyStay/EasyStay.Application/MediatR/RoomTypes/Queries/GetDetails/GetRoomTypeDetailsQueryHandler.cs
@@ -0,0 +1,26 @@
+﻿using AutoMapper;
+using AutoMapper.QueryableExtensions;
+using EasyStay.Application.Common.Exceptions;
+using EasyStay.Application.Interfaces;
+using EasyStay.Application.MediatR.RoomTypes.Queries.Shared;
+using EasyStay.Domain;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace EasyStay.Application.MediatR.RoomTypes.Queries.GetDetails;
+
+public class GetRoomTypeDetailsQueryHandler(
+	IEasyStayDbContext context,
+	IMapper mapper
+) : IRequestHandler<GetRoomTypeDetailsQuery, RoomTypeVm> {
+
+	public async Task<RoomTypeVm> Handle(GetRoomTypeDetailsQuery request, CancellationToken cancellationToken) {
+		var vm = await context.RoomTypes
+			.AsNoTracking()
+			.ProjectTo<RoomTypeVm>(mapper.ConfigurationProvider)
+			.FirstOrDefaultAsync(rt => rt.Id == request.Id, cancellationToken)
+			?? throw new NotFoundException(nameof(RoomType), request.Id);
+
+		return vm;
+	}
+}

688a22eddb94dab9a542ac432976554ca59767ff
Владислав Приходько <3885vlad@gmail.com>
2024-10-06 19:42:44 +0300

    Created RoomVariantsController

---
+++ EasyStay/EasyStay.WebApi/Controllers/RoomVariantsController.cs
@@ -0,0 +1,32 @@
+﻿using EasyStay.Application.MediatR.RoomVariants.Commands.Create;
+using EasyStay.Application.MediatR.RoomVariants.Commands.Delete;
+using EasyStay.Application.MediatR.RoomVariants.Commands.Update;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EasyStay.WebApi.Controllers;
+
+public class RoomVariantsController : BaseApiController {
+	[HttpPost]
+	[Authorize(Roles = "Realtor")]
+	public async Task<IActionResult> Create([FromBody] CreateRoomVariantCommand command) {
+		var id = await Mediator.Send(command);
+		return Ok(id);
+	}
+
+	[HttpPut]
+	[Authorize(Roles = "Realtor")]
+	public async Task<IActionResult> Update([FromBody] UpdateRoomVariantCommand command) {
+		await Mediator.Send(command);
+
+		return NoContent();
+	}
+
+	[HttpDelete("{id}")]
+	[Authorize(Roles = "Realtor")]
+	public async Task<IActionResult> Delete(long id) {
+		await Mediator.Send(new DeleteRoomVariantCommand { Id = id });
+
+		return NoContent();
+	}
+}

b273cb3c2a180c361f33769b62412358dc83d91e
Владислав Приходько <3885vlad@gmail.com>
2024-10-11 20:24:06 +0300

    Created top search menu

--- Booking-front/src/pages/customer/HotelsPage.tsx
+++ Booking-front/src/pages/customer/HotelsPage.tsx
@@ -1,7 +1,21 @@
+import SearchTopSection from "components/partials/customer/SearchTopSection.tsx";
+import VerticalPad from "components/ui/VerticalPad.tsx";
+
 const HotelsPage = () => {
     return (
-        <div>
-            <p>Page</p>
+        <div className="hotels-page">
+            <SearchTopSection />
+
+            <VerticalPad heightPx={18} />
+
+            <div className="page-path">
+                <p className="first-page-path-item page-path-item">Головна</p>
+                <p className="page-path-separator">/</p>
+                <p className="page-path-item">Пошук готелів</p>
+            </div>
+
+            <VerticalPad heightPx={54} />
+
         </div>
     );
 };

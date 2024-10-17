ad70f32f35c2aa053b338382156289fb51b1dd5a
Владислав Приходько <3885vlad@gmail.com>
2024-10-17 18:10:08 +0300

    Created GendersController

---
+++ EasyStay/EasyStay.Application/MediatR/Genders/Queries/Shared/GenderVm.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using EasyStay.Application.Common.Mappings;
+using EasyStay.Domain;
+
+namespace EasyStay.Application.MediatR.Genders.Queries.Shared;
+
+public class GenderVm : IMapWith<Gender> {
+	public long Id { get; set; }
+
+	public string Name { get; set; } = null!;
+
+
+
+	public void Mapping(Profile profile) {
+		profile.CreateMap<Gender, GenderVm>();
+	}
+}

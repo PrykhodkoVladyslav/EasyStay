56ac7c5d40fe721f12861ae4742be8a7c1399324
Владислав Приходько <3885vlad@gmail.com>
2024-10-24 15:24:24 +0300

    Added new migration

--- EasyStay/EasyStay.Persistence/Migrations/20241024122140_added_Bookings_BookingRoomVariants_and_BookingBedSelections_tables.Designer.cs
+++ EasyStay/EasyStay.Persistence/Migrations/20241024122140_added_Bookings_BookingRoomVariants_and_BookingBedSelections_tables.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using EasyStay.Persistence;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace EasyStay.Persistence.Migrations
 {
     [DbContext(typeof(EasyStayDbContext))]
-    partial class EasyStayDbContextModelSnapshot : ModelSnapshot
+    [Migration("20241024122140_added_Bookings_BookingRoomVariants_and_BookingBedSelections_tables")]
+    partial class added_Bookings_BookingRoomVariants_and_BookingBedSelections_tables
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder

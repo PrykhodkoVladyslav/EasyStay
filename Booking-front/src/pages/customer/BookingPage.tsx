aecfb55251ce1e4fbf7fa64e52d4b1f51f1fe6f7
Владислав Приходько <3885vlad@gmail.com>
2024-10-30 19:50:34 +0200

    Created side section.

--- Booking-front/src/pages/customer/BookingPage.tsx
+++ Booking-front/src/pages/customer/BookingPage.tsx
@@ -1,6 +1,7 @@
 import { useParams } from "react-router-dom";
 import "./../../css/booking-page.scss";
 import StageIndicator from "components/partials/customer/StageIndicator.tsx";
+import VerticalPad from "components/ui/VerticalPad.tsx";
 
 export interface IBookingPageExternalInformation {
     hotelId: number;
@@ -31,11 +32,48 @@ const BookingPage = () => {
     const data = JSON.parse(atob(bookingPageExternalInformation)) as IBookingPageExternalInformation;
     console.log(data);
 
-    return (
-        <div className="booking-page-main-container">
-            <StageIndicator options={["Ваш вибір", "Ваші дані", "Завершальний крок"]} currentOptionIndex={1} />
+    // const json = JSON.stringify({
+    //     hotelId: 1,
+    //
+    //     bookingInfo: {
+    //         dateFrom: new Date(),
+    //         dateTo: new Date(),
+    //         bookingRoomVariants: [
+    //             {
+    //                 roomVariantId: 1,
+    //                 quantity: 1,
+    //                 bookingBedSelection: {
+    //                     isSingleBed: true,
+    //                 },
+    //             },
+    //             {
+    //                 roomVariantId: 2,
+    //                 quantity: 2,
+    //                 bookingBedSelection: {
+    //                     isDoubleBed: true,
+    //                     isKingsizeBed: true,
+    //                 },
+    //             },
+    //         ],
+    //     },
+    // } as IBookingPageExternalInformation);
+    // const base64 = btoa(json);
+
+    return <div className="booking-page-main-container">
+        <StageIndicator options={["Ваш вибір", "Ваші дані", "Завершальний крок"]} currentOptionIndex={1} />
+
+        <VerticalPad heightPx={46} />
+
+        <div className="booking-info-container">
+            <div className="side-block">
+                <p>asdasd</p>
+            </div>
+            <div className="center-block">
+                <p>asdasddasd</p>
+
+            </div>
         </div>
-    );
+    </div>;
 };
 
 export default BookingPage;

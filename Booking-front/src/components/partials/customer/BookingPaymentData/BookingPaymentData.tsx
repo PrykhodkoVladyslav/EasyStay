ceb09f965ba4fc408b8b86c201bd8974fea6f6cd
Владислав Приходько <3885vlad@gmail.com>
2024-11-06 16:30:00 +0200

    Created section with bank card information.

--- Booking-front/src/components/partials/customer/BookingPaymentData/BookingPaymentData.tsx
+++ Booking-front/src/components/partials/customer/BookingPaymentData/BookingPaymentData.tsx
@@ -19,6 +19,13 @@ const BookingPaymentData = (props: IBookingPaymentDataProps) => {
     const threeDaysBefore = addDays(dateFrom, -3);
     const formattedThreeDaysBefore = format(threeDaysBefore, "d MMM yyyy", { locale: uk });
 
+    const [holderName, setHolderName] = useState<string>("");
+    const [cardNumber, setCardNumber] = useState<string>("");
+    const [expiryDate, setExpiryDate] = useState<string>("");
+    const [cvc, setCvc] = useState<string>("");
+
+    const [isSaveCard, setIsSaveCard] = useState<boolean>(false);
+
     return <div className="payment-data-container">
         <div className="payment-method-container">
             <div className="payment-title-container">
@@ -74,8 +81,67 @@ const BookingPaymentData = (props: IBookingPaymentDataProps) => {
         {(activeOption === 0 || activeOption === 1) && <>
             <VerticalPad heightPx={40} />
 
-            <div>
+            <div className="bank-card-info-container">
+                <div className="input-container">
+                    <p className="input-title">Імʼя власника картки <sup>*</sup></p>
+                    <input className="input-input"
+                           placeholder="Вказати"
+                           type="text"
+                           value={holderName}
+                           onChange={e => setHolderName(e.target.value)}
+                           maxLength={100}
+                    />
+                </div>
 
+                <div className="input-container">
+                    <p className="input-title">Номер картки <sup>*</sup></p>
+                    <input className="input-input"
+                           placeholder="Вказати"
+                           type="text"
+                           value={cardNumber}
+                           onChange={e => setCardNumber(e.target.value)}
+                           maxLength={16}
+                    />
+                </div>
+
+                <div className="left-right-part">
+                    <div className="input-container">
+                        <p className="input-title">Термін дії <sup>*</sup></p>
+                        <input className="input-input"
+                               placeholder="Вказати"
+                               type="text"
+                               value={expiryDate}
+                               onChange={e => setExpiryDate(e.target.value)}
+                               maxLength={5}
+                        />
+                    </div>
+
+                    <div className="input-container">
+                        <div className="on-sides">
+                            <p className="input-title">CVC - код <sup>*</sup></p>
+                            <img className="pointer" src={getPublicResourceUrl("icons/question/gray-question.svg")}
+                                 alt="question" />
+                        </div>
+                        <input className="input-input"
+                               placeholder="Вказати"
+                               type="text"
+                               value={cvc}
+                               onChange={e => setCvc(e.target.value)}
+                               maxLength={3}
+                        />
+                    </div>
+                </div>
+
+                <div className="save-card-container">
+                    <div
+                        className={`tweaker ${isSaveCard ? "tweaker-enabled" : "tweaker-disabled"} pointer`}
+                        onClick={() => setIsSaveCard(!isSaveCard)}
+                    >
+                        <div />
+                    </div>
+
+                    <p>Зберегти картку для майбутніх платежів</p>
+                </div>
             </div>
         </>}
     </div>;

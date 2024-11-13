f730966654080429c875ff6b2bc8e5392791d6c8
Oleksandr Putsyk <sashaputsyk100903@gmail.com>
2024-11-13 18:53:09 +0200

    Added texts where no data on page

--- Booking-front/src/pages/realtor/ReviewsPage.tsx
+++ Booking-front/src/pages/realtor/ReviewsPage.tsx
@@ -89,32 +89,36 @@ const ReviewsPage = () => {
             </div>
 
             <div className="reviews">
-                {realtorReviews.map((review) => (
-                    <div key={review.id} className="review">
-                        <div className="author">
-                            <img
-                                src={review.author.photo ? `${API_URL}/images/800_${review.author.photo}` : getPublicResourceUrl('account/no_user_photo.png')}
-                                alt=""
-                                className="author-image"
-                            />
-                            <div className="container9">
-                                <p className="name" title={`${review.author.firstName} ${review.author.lastName}`}>
-                                    {review.author.firstName}
-                                </p>
-                                <div className="stars-container">
-                                    <img
-                                        src={getPublicResourceUrl("account/star.svg")}
-                                        alt=""
-                                        className="star"
-                                    />
-                                    <p className="rating">{review.score}</p>
+                {realtorReviews.length > 0 ? (
+                    realtorReviews.map((review) => (
+                        <div key={review.id} className="review">
+                            <div className="author">
+                                <img
+                                    src={review.author.photo ? `${API_URL}/images/800_${review.author.photo}` : getPublicResourceUrl('account/no_user_photo.png')}
+                                    alt=""
+                                    className="author-image"
+                                />
+                                <div className="container9">
+                                    <p className="name" title={`${review.author.firstName} ${review.author.lastName}`}>
+                                        {review.author.firstName}
+                                    </p>
+                                    <div className="stars-container">
+                                        <img
+                                            src={getPublicResourceUrl("account/star.svg")}
+                                            alt=""
+                                            className="star"
+                                        />
+                                        <p className="rating">{review.score}</p>
+                                    </div>
                                 </div>
                             </div>
-                        </div>
 
-                        <p className="description">{review.description}</p>
-                    </div>
-                ))}
+                            <p className="description">{review.description}</p>
+                        </div>
+                    ))
+                ) : (
+                    <p className="isLoading-error">У вас немає Відгуків</p>
+                )}
             </div>
 
             <Pagination

0ac929eba4c33e4617f418d8353532bae895c600
Владислав Приходько <3885vlad@gmail.com>
2024-11-13 14:47:50 +0200

    Added redirection from auth pages

--- Booking-front/src/components/layouts/AuthLayout.tsx
+++ Booking-front/src/components/layouts/AuthLayout.tsx
@@ -1,15 +1,19 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigate } from "react-router-dom";
 import { getPublicResourceUrl } from "utils/publicAccessor.ts";
 import VerticalPad from "components/ui/VerticalPad.tsx";
 import "./../../css/auth-layout.scss";
 
 const AuthLayout = () => {
+    const navigate = useNavigate();
+
     return (
         <main className="auth-layout">
             <div className="half-width flex justify-center">
                 <div className="half-width">
                     <div className="auth-header-block">
-                        <img src={getPublicResourceUrl("logo/logo_EasyStay.svg")} alt="logo" className="auth-logo" />
+                        <img src={getPublicResourceUrl("logo/logo_EasyStay.svg")} alt="logo"
+                             className="auth-logo pointer"
+                             onClick={() => navigate("/")} />
                         <h2 className="auth-header">Відкривай цікаві місця з нами</h2>
                     </div>
                     <VerticalPad heightPx={38} />

bd2b5cb2c221cc110b993845ea5e8e6e15e1e1b5
Владислав Приходько <3885vlad@gmail.com>
2024-11-18 12:58:31 +0200

    Added FavoriteHotels table

---
+++ EasyStay/EasyStay.Domain/FavoriteHotel.cs
@@ -0,0 +1,11 @@
+﻿using EasyStay.Domain.Identity;
+
+namespace EasyStay.Domain;
+
+public class FavoriteHotel {
+	public long HotelId;
+	public Hotel Hotel = null!;
+
+	public long CustomerId;
+	public Customer Customer = null!;
+}
---
+++ EasyStay/EasyStay.Persistence/EntityTypeConfigurations/FavoriteHotelEntityTypeConfiguration.cs
@@ -0,0 +1,16 @@
+﻿using EasyStay.Domain;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace EasyStay.Persistence.EntityTypeConfigurations;
+
+internal class FavoriteHotelEntityTypeConfiguration : IEntityTypeConfiguration<FavoriteHotel> {
+	public void Configure(EntityTypeBuilder<FavoriteHotel> builder) {
+		builder.ToTable("FavoriteHotels");
+
+		builder.HasKey(fh => new {
+			fh.HotelId,
+			fh.CustomerId
+		});
+	}
+}

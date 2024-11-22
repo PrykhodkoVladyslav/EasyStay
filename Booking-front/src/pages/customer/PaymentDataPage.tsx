5f8531db9a5422a8db726445fa47b9baf0b66cf1
Владислав Приходько <3885vlad@gmail.com>
2024-11-22 12:18:54 +0200

    Added loading of bank cards

--- Booking-front/src/pages/customer/PaymentDataPage.tsx
+++ Booking-front/src/pages/customer/PaymentDataPage.tsx
@@ -1,31 +1,46 @@
-import {getPublicResourceUrl} from "utils/publicAccessor.ts";
+import { getPublicResourceUrl } from "utils/publicAccessor.ts";
+import { useDeleteBankCardMutation, useGetAllBankCardsQuery } from "services/bankCard.ts";
+import { format } from "date-fns";
+import showToast from "utils/toastShow.ts";
 
 const PaymentDataPage = () => {
+    const { data: bankCardsData } = useGetAllBankCardsQuery();
+    const [deleteCard, { isLoading }] = useDeleteBankCardMutation();
+
+    const deleteCardHandler = (id: number) => {
+        deleteCard(id).unwrap()
+            .then(() => showToast("Успішно видалено!", "success"))
+            .catch(e => console.log(e));
+    };
 
     return (
         <div className="payment-conteiner">
-        <p className="pre-title">Платіжні дані</p>
+            <p className="pre-title">Платіжні дані</p>
             <h1 className="pre-info">Безпечно додайте або видаліть способи оплати, щоб спростити процес бронювання.</h1>
 
             <div className="card-conteiner">
-            <div className="card-information">
-                <p className="payment-card-p">Платіжні карти</p>
-                <div className="card-num-cont">
-                    <div className="card-type-img" ><img src={getPublicResourceUrl("icons/visa.svg")}></img></div>
-                <p className="card-number">···· 8399</p>
-                </div>
-                <p className="payment-card-p">02-2038</p>
-                <button className="delete-card-btn">Видалити</button>
-            </div>
+                {bankCardsData?.map(card => <div key={card.id} className="card-information">
+                    <p className="payment-card-p">Платіжні карти</p>
+                    <div className="card-num-cont">
+                        <div className="card-type-img">
+                            <img src={getPublicResourceUrl("icons/visa.svg")} alt="card" />
+                        </div>
+                        <p className="card-number">···· {card.number.substring(12)}</p>
+                    </div>
+                    <p className="payment-card-p">{format(card.expirationDate, "MM-yyyy")}</p>
+                    <button className="delete-card-btn" disabled={isLoading}
+                            onClick={() => deleteCardHandler(card.id)}>Видалити
+                    </button>
+                </div>)}
 
-            <div className="add-card-div">
-                <p className="new-card-p">Нова карта</p>
-                <button className="add-card-btn">Додати карту</button>
-            </div>
+                <div className="add-card-div">
+                    <p className="new-card-p">Нова карта</p>
+                    <button className="add-card-btn">Додати карту</button>
+                </div>
             </div>
 
         </div>
     );
-}
+};
 
 export default PaymentDataPage;

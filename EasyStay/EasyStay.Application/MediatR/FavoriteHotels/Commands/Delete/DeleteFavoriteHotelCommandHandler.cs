d904259e703472ac8cf0db957e3023bc2a5b4d36
Владислав Приходько <3885vlad@gmail.com>
2024-11-22 09:44:50 +0200

    Added BankCardsController.DeleteAsync action

--- EasyStay/EasyStay.Application/MediatR/FavoriteHotels/Commands/Delete/DeleteFavoriteHotelCommandHandler.cs
+++ EasyStay/EasyStay.Application/MediatR/FavoriteHotels/Commands/Delete/DeleteFavoriteHotelCommandHandler.cs
@@ -12,11 +12,11 @@ public class DeleteFavoriteHotelCommandHandler(
 ) : IRequestHandler<DeleteFavoriteHotelCommand> {
 
 	public async Task Handle(DeleteFavoriteHotelCommand request, CancellationToken cancellationToken) {
-		var deleted = await context.FavoriteHotels
+		var countOfDeleted = await context.FavoriteHotels
 			.Where(fh => fh.HotelId == request.HotelId && fh.CustomerId == currentUserService.GetRequiredUserId())
 			.ExecuteDeleteAsync(cancellationToken);
 
-		if (deleted == 0)
-			throw new NotFoundException(nameof(FavoriteHotel), request.HotelId);
+		if (countOfDeleted == 0)
+			throw new NotFoundException(nameof(Hotel), request.HotelId);
 	}
 }
--- EasyStay/EasyStay.WebApi/Controllers/BankCardsController.cs
+++ EasyStay/EasyStay.WebApi/Controllers/BankCardsController.cs
@@ -1,4 +1,5 @@
 ﻿using EasyStay.Application.MediatR.BankCards.Commands.Create;
+using EasyStay.Application.MediatR.BankCards.Commands.Delete;
 using EasyStay.Application.MediatR.BankCards.Queries.GetAll;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
@@ -8,16 +9,22 @@ namespace EasyStay.WebApi.Controllers;
 public class BankCardsController : BaseApiController {
 	[HttpGet]
 	[Authorize(Roles = "Customer")]
-	public async Task<IActionResult> GetAll() {
+	public async Task<IActionResult> GetAllAsync() {
 		var items = await Mediator.Send(new GetAllBankCardsQuery());
 		return Ok(items);
 	}
 
 	[HttpPost]
 	[Authorize(Roles = "Customer")]
-	public async Task<IActionResult> Create([FromBody] CreateBankCardCommand command) {
+	public async Task<IActionResult> CreateAsync([FromBody] CreateBankCardCommand command) {
 		var id = await Mediator.Send(command);
-
 		return Ok(id);
+	}
+
+	[HttpDelete]
+	[Authorize(Roles = "Customer")]
+	public async Task<IActionResult> DeleteAsync([FromRoute] int id) {
+		await Mediator.Send(new DeleteBankCardCommand { Id = id });
+		return NoContent();
 	}
 }

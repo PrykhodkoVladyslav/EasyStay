fec3f9c87c8525225ef184bbe142c68c8eb83ab5
Владислав Приходько <3885vlad@gmail.com>
2024-11-22 12:34:51 +0200

    The banking system type icon has been made dynamic

--- Booking-front/src/pages/customer/PaymentDataPage.tsx
+++ Booking-front/src/pages/customer/PaymentDataPage.tsx
@@ -1,4 +1,4 @@
-import { getPublicResourceUrl } from "utils/publicAccessor.ts";
+import { getIconUrl } from "utils/publicAccessor.ts";
 import { useDeleteBankCardMutation, useGetAllBankCardsQuery } from "services/bankCard.ts";
 import { format } from "date-fns";
 import showToast from "utils/toastShow.ts";
@@ -13,6 +13,35 @@ const PaymentDataPage = () => {
             .catch(e => console.log(e));
     };
 
+    function getPaymentSystem(cardNumber: string) {
+        const bin = cardNumber.slice(0, 6);
+
+        if (/^4/.test(bin)) return "Visa";
+        if (/^5[1-5]/.test(bin) || /^2[2-7]/.test(bin)) return "MasterCard";
+        if (/^3[47]/.test(bin)) return "American Express";
+        if (/^6(?:011|5|4[4-9]|22)/.test(bin)) return "Discover";
+        if (/^3(?:6|8|9)/.test(bin)) return "Diners Club";
+        if (/^35(2[89]|[3-8])/.test(bin)) return "JCB";
+        if (/^62/.test(bin)) return "UnionPay";
+
+        return "Unknown";
+    }
+
+    function getCardIconPath(cardNumber: string) {
+        switch (getPaymentSystem(cardNumber)) {
+            case "Visa":
+                return "bankCard/visa.svg";
+            case "MasterCard":
+                return "bankCard/mastercard.svg";
+            default:
+                return "bankCard/other.svg";
+        }
+    }
+
+    function getCardIcon(cardNumber: string) {
+        return getIconUrl(getCardIconPath(cardNumber));
+    }
+
     return (
         <div className="payment-conteiner">
             <p className="pre-title">Платіжні дані</p>
@@ -23,7 +52,7 @@ const PaymentDataPage = () => {
                     <p className="payment-card-p">Платіжні карти</p>
                     <div className="card-num-cont">
                         <div className="card-type-img">
-                            <img src={getPublicResourceUrl("icons/visa.svg")} alt="card" />
+                            <img src={getCardIcon(card.number)} alt="card" />
                         </div>
                         <p className="card-number">···· {card.number.substring(12)}</p>
                     </div>
